e5f0c176623025e7cdf31b0214c94356c66e4bab
heshan sandaruwan <156079550+heshsandaru@users.noreply.github.com>
2026-03-31 12:46:05 +0530

    update application.yaml to change config server URI to localhost

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps : [
+
+    {
+      name   : "student-service",
+      script : "java -jar student-service/target/Student-Service-1.0.0.jar",
+      log_file : "./logs/student-service.log",
+      instances: 1
+    },
+    {
+      name   : "program-service",
+      script : "java -jar program-service/target/Program-Service-1.0.0.jar",
+      log_file: "./logs/program-service.log",
+      instances: 1
+    },
+    {
+      name   : "enrollment-service",
+      script : "java -jar enrollment-service/target/Enrollment-Service-1.0.0.jar",
+      log_file: "./logs/enrollment-service.log",
+      instances: 1
+    }
+  ]
+}
